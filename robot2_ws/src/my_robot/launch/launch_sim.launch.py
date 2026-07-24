c6bc2a0031cca9fab6a25b6a04b2fcb16e4a0d16
joan ouma <joanouma48@gmail.com>
2026-07-24 15:08:24 +0300

    Apply Joan's updates to correct directory structure

--- robot2_ws/src/my_robot/launch/launch_sim.launch.py
+++ robot2_ws/src/my_robot/launch/launch_sim.launch.py
@@ -2,23 +2,17 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, AppendEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
 
-
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='my_robot' #<--- CHANGE ME
+    package_name='my_robot'
 
     dk = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -28,11 +22,24 @@ def generate_launch_description():
     
     world = LaunchConfiguration('world')
 
+    # Explicitly build the full path to your world file inside the package
+    default_world_path = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds',
+        'competition.sdf'
+    )
+
     world_arg = DeclareLaunchArgument(
         'world',
-        default_value="empty.sdf",
-        description='World to load'
+        default_value=default_world_path,
+        description='Full path to world file to load'
         )
+        
+    # Tell Gazebo exactly where to find your custom gamefield model!
+    set_env_vars_resources = AppendEnvironmentVariable(
+            'GZ_SIM_RESOURCE_PATH',
+            os.path.join(get_package_share_directory(package_name), 'worlds', 'models')
+            )
 
     # Include the Gazebo launch file, provided by the ros_gz_sim package
     gazebo = IncludeLaunchDescription(
@@ -41,14 +48,14 @@ def generate_launch_description():
                     launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
              )
 
-    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
+    # Run the spawner node from the ros_gz_sim package.
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
                                    '-name', 'my_robot',
-                                   '-z', '0.1'],
+                                   '-z', '0.5'],
                         output='screen')
               
-    #Launch the ROS-gazebo bridge for normal topics
+    # Launch the ROS-gazebo bridge for normal topics
     bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
     ros_gz_bridge = Node(
         package='ros_gz_bridge',
@@ -60,12 +67,11 @@ def generate_launch_description():
         ]
     )
 
-
-
     # Launch them all!
     return LaunchDescription([
         dk,
         world_arg,
+        set_env_vars_resources,
         gazebo,
         spawn_entity,
         ros_gz_bridge,
