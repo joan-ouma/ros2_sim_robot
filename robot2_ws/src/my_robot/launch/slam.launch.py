0909896e56a556da7b1e2fa8dc35e1ccc9bea240
joan ouma <joanouma48@gmail.com>
2026-08-02 01:37:19 +0300

    feat(launch): implement slam_toolbox launch inclusion and arguments

--- robot2_ws/src/my_robot/launch/slam.launch.py
+++ robot2_ws/src/my_robot/launch/slam.launch.py
@@ -10,5 +10,14 @@ def generate_launch_description():
     # Path to our custom config file
     slam_config_file = os.path.join(my_robot_dir, 'config', 'mapper_params_online_async.yaml')
 
+    # Include the default slam_toolbox launch file and pass our config
+    slam_toolbox = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py'
+        )]),
+        launch_arguments={'slam_params_file': slam_config_file}.items()
+    )
+
     return LaunchDescription([
+        slam_toolbox
     ])
